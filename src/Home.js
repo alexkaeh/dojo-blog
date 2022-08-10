d5c32deef9fc3012599d931b37342c86167d6638
Alex <levviathor@gmail.com>
2022-08-09 21:19:52 -0700

    tutorials 1 thru 16

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {/* skip right if left evaluates to false */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
+
+export default Home;
